28bed749fe6d73db365c3a55348860b7332a8cdc
Jim Hendricks <jhendricks@widen.com>
2012-05-22 11:13:32 -0500

    added css and page mocks

--- src/com/schlock/website/pages/codejam/may2012/Index.java
+++ src/com/schlock/website/pages/codejam/may2012/Index.java
@@ -1,5 +1,8 @@
 package com.schlock.website.pages.codejam.may2012;
 
+import org.apache.tapestry5.annotations.Import;
+
+@Import(stylesheet = "context:layout/codejam/codejam.css")
 public class Index
 {
 }
