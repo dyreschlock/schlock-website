5da2d28417ade24697591d7c6b3911a41334583c
Jim Hendricks <jhendricks@widen.com>
2014-03-16 20:06:28 +0900

    have the overlay displaying, but it's not able to close yet

--- src/com/schlock/website/components/blog/content/ImageGallery.java
+++ src/com/schlock/website/components/blog/content/ImageGallery.java
@@ -2,8 +2,12 @@ package com.schlock.website.components.blog.content;
 
 import com.schlock.website.entities.blog.Post;
 import com.schlock.website.services.blog.PostManagement;
+import org.apache.commons.lang.StringUtils;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import java.util.List;
@@ -19,16 +23,26 @@ public class ImageGallery
     private PostManagement postManagement;
 
 
+    @InjectComponent
+    private Zone imageOverlayZone;
+
+
     @Property
     private Integer currentIndex;
 
     @Property
     private String currentImage;
 
+    @Property
+    @Persist
+    private String selectedImage;
+
 
 
     public List<String> getGalleryImages()
     {
+        selectedImage = null;
+
         List<String> gallery = postManagement.getGalleryImages(post);
         return gallery;
     }
@@ -36,5 +50,17 @@ public class ImageGallery
     public boolean isNewLine()
     {
         return currentIndex % 4 == 0;
+    }
+
+    public boolean isHasImageSelected()
+    {
+        return StringUtils.isNotBlank(selectedImage);
+    }
+
+    Object onSelectImage(String image)
+    {
+        this.selectedImage = image;
+
+        return imageOverlayZone;
     }
 }
