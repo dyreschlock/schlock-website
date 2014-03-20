e4d40092fe97365bd3a0d0a4b64518360cf9fd8b
Jim Hendricks <jhendricks@widen.com>
2014-03-20 16:01:55 +0900

    fixing up the may 2012 code jam

--- src/com/schlock/website/components/Layout.java
+++ src/com/schlock/website/components/Layout.java
@@ -1,10 +1,8 @@
 package com.schlock.website.components;
 
-import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 
-@Import(stylesheet = "context:layout/site.css")
 public class Layout
 {
     @Parameter(required = true)
