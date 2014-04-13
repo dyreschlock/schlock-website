876c10fc2c962999bbe0a48d97b73777f42c992f
Jim Hendricks <jhendricks@widen.com>
2014-04-13 22:29:37 +0900

    rearranging class structure for posts and creating a page class // #5

---
+++ src/com/schlock/website/entities/blog/Post.java
@@ -0,0 +1,19 @@
+package com.schlock.website.entities.blog;
+
+public class Post extends AbstractPost
+{
+
+    protected Post()
+    {
+    }
+
+    public Post(String uuid)
+    {
+        super(uuid);
+    }
+
+    public boolean isPage()
+    {
+        return false;
+    }
+}
