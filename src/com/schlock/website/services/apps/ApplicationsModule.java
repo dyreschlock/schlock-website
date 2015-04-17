1a844988a93179e0756fb62e68cc5cf28d5b55e3
Jim Hendricks <jhendricks@widen.com>
2015-04-17 09:18:17 +0900

    binding the service // #19

--- src/com/schlock/website/services/apps/ApplicationsModule.java
+++ src/com/schlock/website/services/apps/ApplicationsModule.java
@@ -1,5 +1,7 @@
 package com.schlock.website.services.apps;
 
+import com.schlock.website.services.apps.notfibbage.NotfibbageManagement;
+import com.schlock.website.services.apps.notfibbage.impl.NotfibbageManagementImpl;
 import com.schlock.website.services.apps.subtitles.SubtitleFixerService;
 import com.schlock.website.services.apps.subtitles.impl.SubtitleFixerServiceImpl;
 import org.apache.tapestry5.ioc.ServiceBinder;
@@ -9,5 +11,6 @@ public class ApplicationsModule
     public static void bind(ServiceBinder binder)
     {
         binder.bind(SubtitleFixerService.class, SubtitleFixerServiceImpl.class);
+        binder.bind(NotfibbageManagement.class, NotfibbageManagementImpl.class);
     }
 }
