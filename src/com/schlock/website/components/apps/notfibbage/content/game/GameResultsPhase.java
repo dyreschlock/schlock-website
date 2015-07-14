7a8de119158abfc7e534a6e588eb79652f10b06f
Jim Hendricks <jhendricks@widen.com>
2015-07-14 14:09:45 +0900

    adding next links // #19

--- src/com/schlock/website/components/apps/notfibbage/content/game/GameResultsPhase.java
+++ src/com/schlock/website/components/apps/notfibbage/content/game/GameResultsPhase.java
@@ -1,5 +1,20 @@
 package com.schlock.website.components.apps.notfibbage.content.game;
 
+import com.schlock.website.pages.apps.notfibbage.Game;
+import com.schlock.website.services.apps.notfibbage.NotFibbageController;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
 public class GameResultsPhase
 {
+    @Inject
+    private NotFibbageController controller;
+
+
+
+    Object onNext()
+    {
+        controller.next();
+
+        return Game.class;
+    }
 }
