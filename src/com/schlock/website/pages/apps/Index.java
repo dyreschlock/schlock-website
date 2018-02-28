39247c2db25fe3ef2a65a9801614dab665303905
Jim Hendricks <jhendricks@widen.com>
2018-02-28 13:37:16 +0900

    added a simple redirect for /apps going to /project // #28

---
+++ src/com/schlock/website/pages/apps/Index.java
@@ -0,0 +1,11 @@
+package com.schlock.website.pages.apps;
+
+import com.schlock.website.pages.Projects;
+
+public class Index
+{
+    Object onActivate()
+    {
+        return Projects.class;
+    }
+}
