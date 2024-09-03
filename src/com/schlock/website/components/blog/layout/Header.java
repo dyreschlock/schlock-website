c72243b00a4288a77b52e91ea732907cb609813f
Jim Hendricks <dyreschlock@gmail.com>
2024-09-03 17:32:29 +0900

    The menu has been added for mobile // #87

--- src/com/schlock/website/components/blog/layout/Header.java
+++ src/com/schlock/website/components/blog/layout/Header.java
@@ -1,5 +1,25 @@
 package com.schlock.website.components.blog.layout;
 
+import com.schlock.website.entities.blog.AbstractCategory;
+import com.schlock.website.entities.blog.PostCategory;
+import com.schlock.website.services.database.blog.CategoryDAO;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.util.List;
+
 public class Header
 {
+    @Inject
+    private CategoryDAO categoryDAO;
+
+    @Property
+    private AbstractCategory currentCategory;
+
+
+    public List<PostCategory> getCategories()
+    {
+        return categoryDAO.getTopInOrder();
+    }
+
 }
