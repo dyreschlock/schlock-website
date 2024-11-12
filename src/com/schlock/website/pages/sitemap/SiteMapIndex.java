964b4be186cfc919da3d8c98c8d42094e158419c
Jim Hendricks <dyreschlock@gmail.com>
2024-11-12 20:15:33 +0900

    archive and category pages have been added to the sitemap // #139

--- src/com/schlock/website/pages/sitemap/SiteMapIndex.java
+++ src/com/schlock/website/pages/sitemap/SiteMapIndex.java
@@ -1,8 +1,10 @@
 package com.schlock.website.pages.sitemap;
 
+import com.schlock.website.entities.blog.PostCategory;
 import com.schlock.website.entities.blog.ProjectCategory;
 import com.schlock.website.services.DateFormatter;
 import com.schlock.website.services.DeploymentContext;
+import com.schlock.website.services.blog.PostArchiveManagement;
 import com.schlock.website.services.database.blog.CategoryDAO;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
@@ -16,6 +18,9 @@ public class SiteMapIndex
 {
     @Inject
     private CategoryDAO categoryDAO;
+
+    @Inject
+    private PostArchiveManagement archiveManagement;
 
     @Inject
     private DateFormatter dateFormatter;
@@ -51,11 +56,60 @@ public class SiteMapIndex
     //course pages
     //regular pages
 
-    //archive pages
-    //category pages
-    //project pages
 
-    public List<String> getProjectPages()
+
+
+    public List<String> getDirectoryPages()
+    {
+        List<String> pages = new ArrayList<>();
+
+        pages.addAll(getArchivePages());
+        pages.addAll(getCategoryPages());
+        pages.addAll(getProjectPages());
+        return pages;
+    }
+
+    private List<String> getArchivePages()
+    {
+        final String ARCHIVE = "archive";
+
+        List<String> pages = new ArrayList<>();
+
+        pages.add(ARCHIVE + "/index.html");
+        for(String year : archiveManagement.getYearlyMonthlyIterations(null, null))
+        {
+            pages.add(ARCHIVE + "/" + year + "/index.html");
+
+            Integer y = Integer.parseInt(year);
+            for(String yearMonth : archiveManagement.getYearlyMonthlyIterations(y, null))
+            {
+                String path = yearMonth.replace("-", "/");
+
+                pages.add(ARCHIVE + "/" + path + ".html");
+            }
+        }
+        return pages;
+    }
+
+    private List<String> getCategoryPages()
+    {
+        final String CATEGORY = "category";
+
+        List<String> pages = new ArrayList<>();
+
+        for(PostCategory cat : categoryDAO.getTopInOrder())
+        {
+            pages.add(CATEGORY + "/" + cat.getUuid() + ".html");
+
+            for(PostCategory sub : categoryDAO.getSubInOrder(cat.getId()))
+            {
+                pages.add(CATEGORY + "/" + sub.getUuid() + ".html");
+            }
+        }
+        return pages;
+    }
+
+    private List<String> getProjectPages()
     {
         final String PROJECTS = "projects";
 
