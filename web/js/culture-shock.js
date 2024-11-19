f55a4fabf40cb1a47ade344b0f904a2b38862a6a
Jim Hendricks <dyreschlock@gmail.com>
2024-11-19 15:01:56 +0900

    the curve is now painting correctly // #138

--- web/js/culture-shock.js
+++ web/js/culture-shock.js
@@ -8,24 +8,52 @@ var b2 = 'rgba(195, 195, 227, 0.5)';
 var b3 = 'rgba(195, 217, 227, 0.5)';
 var b4 = 'rgba(195, 227, 195, 0.5)';
 
-const time =      [ 1, 2, 3, 4, 5, 6, 7,   8,   9,   10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,  22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-const curve =     [ 0, 4, 7, 9,11, 8, 2,-3.5,-9.5,-11.5,-12,-10, -8, -6, -3,  1,  3,  2,  1, -2, -3,-2.5, -1,  0,  1,  0, -1,  0,0.3,  0,  0];
-const lineColor = [c1,c1,c1,c1,c1,c2,c2,  c2,  c2,   c2, c3, c3, c3, c3, c4, c4, c4, c4, c4, c4, c4,  c4, c4, c4, c4, c4, c4, c4, c4, c4, c4];
-const backColor = [b1,b1,b1,b1,b1,b2,b2,  b2,  b2,   b2, b3, b3, b3, b3, b4, b4, b4, b4, b4, b4, b4,  b4, b4, b4, b4, b4, b4, b4, b4, b4, b4];
+const time =      [   1,   2,   3,   4,   5, 6, 7,   8,   9,   10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,  22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const p1_curve  = [   0,   4,   7,   9,  11];
+const p2_curve  = [null,null,null,null,  11,   8,  2, -3.5,-9.5,-11.5];
+const p3_curve  = [null,null,null,null,null,null,null,null,null,-11.5,-12, -10,  -8,  -6, -3];
+const p4_curve  = [null,null,null,null,null,null,null,null,null,null,null,null,null,null, -3,  1,  3,  2,  1, -2, -3,-2.5, -1,  0,  1,  0, -1,  0,0.3,  0,  0];
 
+const curve =     [ 0, 4, 7, 9,11, 8, 2,-3.5,-9.5,-11.5,-12,-10, -8, -6, -3,  1,  3,  2,  1, -2, -3,-2.5, -1,  0,  1,  0, -1,  0,0.3,  0,  0];
 
 new Chart("fullChart", {
     type: "line",
     data: {
         labels: time,
-        datasets: [{
-            data: curve,
-            borderColor: lineColor,
-            backgroundColor: backColor,
-            borderWidth: 5,
-            pointRadius: 0,
-            fill: true,
-        }]
+        datasets: [
+            {
+                data: p1_curve,
+                borderColor: c1,
+                backgroundColor: b1,
+                borderWidth: 5,
+                pointRadius: 0,
+                fill: true,
+            },
+            {
+                data: p2_curve,
+                borderColor: c2,
+                backgroundColor: b2,
+                borderWidth: 5,
+                pointRadius: 0,
+                fill: true,
+            },
+            {
+                data: p3_curve,
+                borderColor: c3,
+                backgroundColor: b3,
+                borderWidth: 5,
+                pointRadius: 0,
+                fill: true,
+            },
+            {
+                data: p4_curve,
+                borderColor: c4,
+                backgroundColor: b4,
+                borderWidth: 5,
+                pointRadius: 0,
+                fill: true,
+            },
+        ]
     },
     options: {
         legend: {
