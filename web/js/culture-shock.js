57f127a9029ea855b4bf320b813e532603bacfed
Jim Hendricks <dyreschlock@gmail.com>
2024-11-19 14:21:24 +0900

    changing the curve slightly and removing labels // #138

--- web/js/culture-shock.js
+++ web/js/culture-shock.js
@@ -1,5 +1,5 @@
-const time =  [1,2,3,4, 5,6,7,   8,   9,   10, 11, 12,13,14,15,16,17,18,19,20,21,  22,23,24,25,26,27,28, 29,30];
-const curve = [0,4,7,9,11,8,2,-3.5,-9.5,-11.5,-12,-10,-8,-6,-3, 1, 3, 2, 1,-2,-3,-2.5,-1, 0, 1, 0,-1, 0,0.3, 0];
+const time =  [1,2,3,4, 5,6,7,   8,   9,   10, 11, 12,13,14,15,16,17,18,19,20,21,  22,23,24,25,26,27,28, 29,30,31];
+const curve = [0,4,7,9,11,8,2,-3.5,-9.5,-11.5,-12,-10,-8,-6,-3, 1, 3, 2, 1,-2,-3,-2.5,-1, 0, 1, 0,-1, 0,0.3, 0, 0];
 
 new Chart("fullChart", {
     type: "line",
@@ -20,14 +20,21 @@ new Chart("fullChart", {
         },
         scales: {
             xAxes:[{
+                gridLines: {
+                    color: "#ccc",
+                },
                 ticks: {
-                    display: false
+                    display: false,
                 }
             }],
             yAxes: [{
+                gridLines: {
+                    color: "#ccc",
+                    zeroLineWidth: 2,
+                    zeroLineColor: "black",
+                },
                 ticks: {
                     display: false,
-                    beginAtZero: true
                 }
             }]
         }
