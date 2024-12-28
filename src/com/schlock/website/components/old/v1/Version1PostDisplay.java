4094bc7c6f3a1c00cb739fa8285681247ff6d5d0
Jim Hendricks <dyreschlock@gmail.com>
2024-12-28 12:17:33 +0900

    adding image gallery to posts // #156

---
+++ src/com/schlock/website/components/old/v1/Version1PostDisplay.java
@@ -0,0 +1,100 @@
+package com.schlock.website.components.old.v1;
+
+import com.schlock.website.entities.blog.AbstractPost;
+import com.schlock.website.entities.blog.Image;
+import com.schlock.website.entities.old.SiteVersion;
+import com.schlock.website.services.DateFormatter;
+import com.schlock.website.services.blog.ImageManagement;
+import com.schlock.website.services.blog.PostManagement;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.util.List;
+
+public class Version1PostDisplay
+{
+    @Parameter(required = true)
+    @Property
+    private AbstractPost post;
+
+    @Inject
+    private PostManagement postManagement;
+
+    @Inject
+    private ImageManagement imageManagement;
+
+    @Inject
+    private DateFormatter dateFormatter;
+
+
+    public String getPostTitle()
+    {
+        return post.getTitle();
+    }
+
+    public String getPostBodyHTML()
+    {
+        String html = postManagement.generatePostHTML(post, SiteVersion.V1);
+        return html;
+    }
+
+    public boolean isHasDate()
+    {
+        return post != null && post.isPost() && post.getCreated() != null;
+    }
+
+    public String getPostDate()
+    {
+        return dateFormatter.shortDateFormat(post.getCreated());
+    }
+
+    public String getImagesTableHTML()
+    {
+        final String TR_START = "<tr align=\"center\" valign=\"middle\" bgcolor=\"#FFFFFF\"> ";
+        final String TR_END = "</tr>";
+
+        final String WIDTH = "16.5%";
+        final String TD_ENTRY = "<td width=\"%s\" height=\"110\"><a href=\"%s\"><img class=\"galleryImage\" src=\"%s\" border=\"0\"></a></td>";
+
+        final String TD_EMPTY = "<td width=\"17%\" height=\"110\"><font>&nbsp;</font></td>";
+
+        final int COL_MAX = 6;
+
+
+        StringBuilder sb = new StringBuilder();
+        int col = 0;
+
+        sb.append(TR_START);
+
+        List<Image> images = imageManagement.getGalleryImages(post);
+        for(Image image : images)
+        {
+            if (col == COL_MAX)
+            {
+                sb.append(TR_END).append(TR_START);
+                col = 0;
+            }
+
+            String thumbnail = image.getImageLink();
+            String link = image.getImageLink();
+            if (image.getParent() != null)
+            {
+                link = image.getParent().getImageLink();
+            }
+
+            String td = String.format(TD_ENTRY, WIDTH, link, thumbnail);
+            sb.append(td);
+
+            col++;
+        }
+        while(col < COL_MAX)
+        {
+            sb.append(TD_EMPTY);
+            col++;
+        }
+        sb.append(TR_END);
+
+        return sb.toString();
+    }
+}
--- src/com/schlock/website/pages/old/v1/V1Index.java
+++ src/com/schlock/website/pages/old/v1/V1Index.java
@@ -1,29 +1,31 @@
 package com.schlock.website.pages.old.v1;
 
 import com.schlock.website.entities.blog.AbstractPost;
+import com.schlock.website.entities.blog.Page;
 import com.schlock.website.entities.old.SiteVersion;
 import com.schlock.website.pages.old.AbstractOldVersionIndex;
-import com.schlock.website.services.DateFormatter;
 import com.schlock.website.services.DeploymentContext;
 import com.schlock.website.services.blog.CssCache;
-import com.schlock.website.services.blog.PostManagement;
 import com.schlock.website.services.database.blog.PostDAO;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PageRenderLinkSource;
 
 public class V1Index extends AbstractOldVersionIndex
 {
+    private static final String ARCHIVE_PAGE = "archive";
+    private static final String PROJECTS_PAGE = "projects";
+    private static final String GAMES_PAGE = "review";
+    private static final String MUSIC_PAGE = "music";
+
+    @Inject
+    private PageRenderLinkSource linkSource;
+
     @Inject
     private DeploymentContext context;
 
     @Inject
-    private DateFormatter dateFormatter;
-
-    @Inject
     private CssCache cssCache;
-
-    @Inject
-    private PostManagement postManagement;
 
     @Inject
     private PostDAO postDAO;
@@ -53,6 +55,31 @@ public class V1Index extends AbstractOldVersionIndex
     }
 
 
+    public String getArchiveLink()
+    {
+        return linkSource.createPageRenderLinkWithContext(V1Index.class, ARCHIVE_PAGE).toURI();
+    }
+
+    public String getAboutMeLink()
+    {
+        return linkSource.createPageRenderLinkWithContext(V1Index.class, Page.ABOUT_ME_UUID).toURI();
+    }
+
+    public String getProjectsLink()
+    {
+        return linkSource.createPageRenderLinkWithContext(V1Index.class, PROJECTS_PAGE).toURI();
+    }
+
+    public String getGamesLink()
+    {
+        return linkSource.createPageRenderLinkWithContext(V1Index.class, GAMES_PAGE).toURI();
+    }
+
+    public String getMusicLink()
+    {
+        return linkSource.createPageRenderLinkWithContext(V1Index.class, MUSIC_PAGE).toURI();
+    }
+
     public String getImageLinkPopular()
     {
         String link = context.webDomain() + "img/old/popular.gif";
@@ -63,26 +90,5 @@ public class V1Index extends AbstractOldVersionIndex
     {
         String link = context.webDomain() + "img/old/pic4.jpg";
         return link;
-    }
-
-    public String getPostTitle()
-    {
-        return post.getTitle();
-    }
-
-    public String getPostBodyHTML()
-    {
-        String html = postManagement.generatePostHTML(post, SiteVersion.V1);
-        return html;
-    }
-
-    public boolean isHasDate()
-    {
-        return post != null && post.getCreated() != null;
-    }
-
-    public String getPostDate()
-    {
-        return dateFormatter.shortDateFormat(post.getCreated());
     }
 }
