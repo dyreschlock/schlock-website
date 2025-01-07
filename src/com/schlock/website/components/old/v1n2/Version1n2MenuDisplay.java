21238d24fd635b03dd4afaaf26660f832ac83aa2
Jim Hendricks <dyreschlock@gmail.com>
2025-01-07 12:17:33 +0900

    adding the menu to v1 // #156

---
+++ src/com/schlock/website/components/old/v1n2/Version1n2MenuDisplay.java
@@ -0,0 +1,58 @@
+package com.schlock.website.components.old.v1n2;
+
+import com.schlock.website.entities.blog.Page;
+import com.schlock.website.entities.old.SiteVersion;
+import com.schlock.website.pages.old.v2.V2Index;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PageRenderLinkSource;
+
+public class Version1n2MenuDisplay
+{
+    @Inject
+    private PageRenderLinkSource linkSource;
+
+    @Parameter(required=true)
+    private SiteVersion version;
+
+
+    public boolean isV1()
+    {
+        return SiteVersion.V1.equals(version);
+    }
+
+    public boolean isV2()
+    {
+        return SiteVersion.V2.equals(version);
+    }
+
+
+    public String getHomeLink()
+    {
+        return linkSource.createPageRenderLink(version.indexClass()).toURI();
+    }
+
+    public String getArchiveLink()
+    {
+        String param = V2Index.ARCHIVE_PAGE;
+        return linkSource.createPageRenderLinkWithContext(version.indexClass(), param).toURI();
+    }
+
+    public String getAboutMeLink()
+    {
+        String param = Page.ABOUT_ME_UUID;
+        return linkSource.createPageRenderLinkWithContext(version.indexClass(), param).toURI();
+    }
+
+    public String getProjectsLink()
+    {
+        String param = V2Index.PROJECTS_PAGE;
+        return linkSource.createPageRenderLinkWithContext(version.indexClass(), param).toURI();
+    }
+
+    public String getGamesLink()
+    {
+        String param = V2Index.GAMES_PAGE;
+        return linkSource.createPageRenderLinkWithContext(version.indexClass(), param).toURI();
+    }
+}
