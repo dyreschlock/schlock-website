6eb680a34a0042b5be1c11b5915be9ceb7c92c82
Jim Hendricks <dyreschlock@gmail.com>
2025-01-07 14:27:25 +0900

    fixing the colors and using a layout // #156

---
+++ src/com/schlock/website/components/old/v1/LayoutVersion1.java
@@ -0,0 +1,34 @@
+package com.schlock.website.components.old.v1;
+
+import com.schlock.website.entities.blog.AbstractPost;
+import com.schlock.website.entities.old.SiteVersion;
+import com.schlock.website.services.blog.CssCache;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.util.List;
+
+public class LayoutVersion1
+{
+    @Parameter(required = true)
+    private List<AbstractPost> posts;
+
+    @Parameter(required = true)
+    private String page;
+
+    @Inject
+    private CssCache cssCache;
+
+
+
+
+    public String getPageCss()
+    {
+        return cssCache.getCssOldVersions(posts, getVersion());
+    }
+
+    public SiteVersion getVersion()
+    {
+        return SiteVersion.V1;
+    }
+}
