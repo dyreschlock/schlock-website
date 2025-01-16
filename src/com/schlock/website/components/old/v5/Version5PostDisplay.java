448748865344f1c699a4ab61f0842617750f5376
Jim Hendricks <dyreschlock@gmail.com>
2025-01-16 16:22:15 +0900

    adding about me post // #156

--- src/com/schlock/website/components/old/v5/Version5PostDisplay.java
+++ src/com/schlock/website/components/old/v5/Version5PostDisplay.java
@@ -1,10 +1,16 @@
 package com.schlock.website.components.old.v5;
 
 import com.schlock.website.components.old.AbstractOldPostDisplay;
+import com.schlock.website.entities.blog.Page;
 import com.schlock.website.entities.old.SiteVersion;
 
 public class Version5PostDisplay extends AbstractOldPostDisplay
 {
+    public boolean isAboutMe()
+    {
+        return getPost() != null && Page.ABOUT_ME_UUID.equals(getPost().getUuid());
+    }
+
     protected SiteVersion getVersion()
     {
         return SiteVersion.V5;
--- src/com/schlock/website/components/old/v5/Version5SiteMapPanel.java
+++ src/com/schlock/website/components/old/v5/Version5SiteMapPanel.java
@@ -1,10 +1,24 @@
 package com.schlock.website.components.old.v5;
 
 import com.schlock.website.components.old.AbstractOldLinks;
+import com.schlock.website.entities.blog.AbstractPost;
 import com.schlock.website.entities.old.SiteVersion;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
 
 public class Version5SiteMapPanel extends AbstractOldLinks
 {
+    @Parameter(required = true)
+    @Property
+    private AbstractPost post;
+
+
+    public boolean isHasPost()
+    {
+        return post != null;
+    }
+
+
     public SiteVersion getVersion()
     {
         return SiteVersion.V5;
