84b7480aa798d4b6fefb7c3d73c045b0c2a4f703
Jim Hendricks <dyreschlock@gmail.com>
2025-02-01 21:08:54 +0900

    text updates for v2 pages // #163

--- src/com/schlock/website/pages/old/v2/V2Radio.java
+++ src/com/schlock/website/pages/old/v2/V2Radio.java
@@ -1,7 +1,13 @@
 package com.schlock.website.pages.old.v2;
 
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PageRenderLinkSource;
+
 public class V2Radio extends AbstractVersion2Page
 {
+    @Inject
+    private PageRenderLinkSource linkSource;
+
     public String getPage()
     {
         return RADIO_PAGE;
@@ -9,6 +15,6 @@ public class V2Radio extends AbstractVersion2Page
 
     public String getEventLink()
     {
-        return ""; // dave-ralph-the-quest
+        return linkSource.createPageRenderLinkWithContext(V2Projects.class, "dave-ralph-the-quest").toURI();
     }
 }
