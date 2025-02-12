76074b736b18f9bec382e3f700f279aa60e3b216
Jim Hendricks <dyreschlock@gmail.com>
2025-02-13 07:44:25 +0900

    fixing links to photo page in old pages // #162

--- src/com/schlock/website/services/blog/impl/PostManagementImpl.java
+++ src/com/schlock/website/services/blog/impl/PostManagementImpl.java
@@ -22,7 +22,7 @@ public class PostManagementImpl implements PostManagement
     private final static int PREVIEW_LENGTH = 900;
     private final static int POST_DESCRIPTION_LENGTH = 160;
 
-    private static final List<String> EXCLUDE_LINKS_FROM_OLD_PAGE_CHANGES = Arrays.asList("site-design-history", "today", "errorpage");
+    private static final List<String> EXCLUDE_LINKS_FROM_OLD_PAGE_CHANGES = Arrays.asList("site-design-history", "today", "errorpage", "photo");
 
     private final static String BREAK = "<break>";
 
