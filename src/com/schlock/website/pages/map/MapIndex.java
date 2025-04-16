71e6dfceea3e80eeaab580b09760f3fa73be9772
Jim Hendricks <dyreschlock@gmail.com>
2025-04-16 11:00:15 +0900

    setting up the base pages for the map // #185

---
+++ src/com/schlock/website/pages/map/MapIndex.java
@@ -0,0 +1,27 @@
+package com.schlock.website.pages.map;
+
+import com.schlock.website.entities.blog.Page;
+import com.schlock.website.services.database.blog.PostDAO;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+public class MapIndex
+{
+    @Inject
+    private PostDAO postDAO;
+
+    private Page cachedPage;
+
+    public Page getPage()
+    {
+        if (cachedPage == null)
+        {
+            cachedPage = (Page) postDAO.getByUuid(Page.MAP_UUID);
+        }
+        return cachedPage;
+    }
+
+    public String getPageDescription()
+    {
+        return getPage().getBlurb();
+    }
+}
