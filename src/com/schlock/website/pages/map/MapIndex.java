1998aaa562deda4345dae50d17bf719314e26cf6
Jim Hendricks <dyreschlock@gmail.com>
2025-04-24 16:29:14 +0900

    adding icon descriptions to the map page // #187 #185

--- src/com/schlock/website/pages/map/MapIndex.java
+++ src/com/schlock/website/pages/map/MapIndex.java
@@ -1,17 +1,26 @@
 package com.schlock.website.pages.map;
 
+import com.schlock.website.entities.blog.LocationType;
 import com.schlock.website.entities.blog.Page;
 import com.schlock.website.services.blog.MapLocationManagement;
 import com.schlock.website.services.database.blog.PostDAO;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class MapIndex
 {
     @Inject
+    private Messages messages;
+
+    @Inject
     private MapLocationManagement mapManagement;
 
     @Inject
     private PostDAO postDAO;
+
+    @Property
+    private LocationType currentMarker;
 
     private Page cachedPage;
 
@@ -32,5 +41,30 @@ public class MapIndex
     public String getJavascript()
     {
         return mapManagement.generateMapJavascript();
+    }
+
+    public LocationType[] getMarkers()
+    {
+        return LocationType.values();
+    }
+
+    public String getMarkerPath()
+    {
+        return currentMarker.getIconPath();
+    }
+
+    public String getMarkerName()
+    {
+        return messages.get(currentMarker.name());
+    }
+
+    public String getMarkerDescription()
+    {
+        String name = messages.get(currentMarker.name());
+        String desc = messages.get(currentMarker.name() + "Desc");
+
+        String DESC = "<b>%s</b>: %s";
+
+        return String.format(DESC, name, desc);
     }
 }
