e554f2a9f88df2d35acea0b032272d2314a0c5ca
Jim Hendricks <dyreschlock@gmail.com>
2025-10-20 15:17:50 +0900

    adding category details to keywords // #192

--- src/com/schlock/website/entities/blog/Keyword.java
+++ src/com/schlock/website/entities/blog/Keyword.java
@@ -7,6 +7,14 @@ public class Keyword extends Persisted
     public static final String DELIMITER = ",";
 
     private String name;
+    private String title;
+    private String description;
+
+    private KeywordType type;
+
+    private Keyword parent;
+    private Integer ordering;
+
 
     protected Keyword()
     {
@@ -18,6 +26,14 @@ public class Keyword extends Persisted
     }
 
 
+    public boolean isVisible()
+    {
+        return type != null && type.equals(KeywordType.VISIBLE);
+    }
+
+
+
+
     public String getName()
     {
         return name;
@@ -26,5 +42,55 @@ public class Keyword extends Persisted
     public void setName(String name)
     {
         this.name = name;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public KeywordType getType()
+    {
+        return type;
+    }
+
+    public void setType(KeywordType type)
+    {
+        this.type = type;
+    }
+
+    public Keyword getParent()
+    {
+        return parent;
+    }
+
+    public void setParent(Keyword parent)
+    {
+        this.parent = parent;
+    }
+
+    public Integer getOrdering()
+    {
+        return ordering;
+    }
+
+    public void setOrdering(Integer ordering)
+    {
+        this.ordering = ordering;
     }
 }
---
+++ src/com/schlock/website/entities/blog/KeywordType.java
@@ -0,0 +1,8 @@
+package com.schlock.website.entities.blog;
+
+public enum KeywordType
+{
+    VISIBLE,
+    PROJECT,
+    COURSE;
+}
